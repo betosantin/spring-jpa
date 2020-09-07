bc297032b6cdc31f8131605e07d5f4d983b981fc
Roberto Santin <betosantin@gmail.com>
2020-09-07 16:12:23 -0300

    Script, entidades e db sql

--- src/main/java/br/com/robertosantin/spring/jpa/VendasApplication.java
+++ src/main/java/br/com/robertosantin/spring/jpa/VendasApplication.java
@@ -1,16 +1,54 @@
 package br.com.robertosantin.spring.jpa;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.robertosantin.spring.jpa.entity.Cliente;
+import br.com.robertosantin.spring.jpa.repository.Clientes;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
 	
+	@Bean
+	public CommandLineRunner init(@Autowired Clientes clientes) {
+		
+		return args -> {
+			clientes.salvar(new Cliente("Roberto S"));
+			clientes.salvar(new Cliente("Teste"));
+			
+			List<Cliente> clis = clientes.obterTodos();
+			clis.forEach(System.out::println);
+			
+			clis.forEach(c -> {
+				c.setNome(c.getNome() + " atualizado");
+				clientes.atualizar(c);
+			});
+			
+			clis = clientes.obterTodos();
+			clis.forEach(System.out::println);
+			
+			clis = clientes.obterTodos();
+			clis.forEach(c -> {
+				clientes.deletar(c);
+			});
+			
+			clis = clientes.obterTodos();
+			clis.forEach(System.out::println);
+		};
+		
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
 	}
+	
 	
 }
---
+++ src/main/java/br/com/robertosantin/spring/jpa/repository/Clientes.java
@@ -0,0 +1,64 @@
+package br.com.robertosantin.spring.jpa.repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.robertosantin.spring.jpa.entity.Cliente;
+
+@Transactional
+@Repository
+public class Clientes {
+
+	private static String INSERT = "INSERT INTO CLIENTE (NOME) VALUES (?) ";
+	private static String UPDATE = "UPDATE CLIENTE SET NOME = ?  WHERE ID = ? ";
+	private static String DELETE = "DELETE FROM CLIENTE WHERE ID = ? ";
+	private static String SELECT_ALL = "SELECT * FROM CLIENTE ";
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public Cliente salvar(Cliente cliente) {
+		jdbcTemplate.update(INSERT, new Object[] { cliente.getNome() });
+
+		return cliente;
+	}
+
+	public Cliente atualizar(Cliente cliente) {
+		jdbcTemplate.update(UPDATE, new Object[] { cliente.getNome(), cliente.getId() });
+
+		return cliente;
+	}
+
+	public void deletar(Cliente cliente) {
+		this.deletar(cliente.getId());
+	}
+
+	public void deletar(Integer id) {
+		jdbcTemplate.update(DELETE, new Object[] { id });
+	}
+
+	public List<Cliente> buscarPorNome(String nome) {
+		return jdbcTemplate.query(SELECT_ALL.concat(" WHERE NOME like ?"), new Object[] { "%" + nome + "%" },
+				clienteMapper());
+	}
+
+	public List<Cliente> obterTodos() {
+		return jdbcTemplate.query(SELECT_ALL, clienteMapper());
+	}
+
+	private RowMapper<Cliente> clienteMapper() {
+		return new RowMapper<Cliente>() {
+			@Override
+			public Cliente mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return new Cliente(rs.getInt("id"), rs.getString("nome"));
+			}
+		};
+	}
+}
